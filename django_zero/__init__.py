eb6e77dd8de326eb149e287c2a07d0098422cbb2
Romain Dorgueil <romain@dorgueil.net>
2018-02-03 16:06:35 +0100

    Adds version to main package.

--- django_zero/__init__.py
+++ django_zero/__init__.py
@@ -1,6 +1,8 @@
 import inspect
 import os
 
+from django_zero._version import __version__
+
 DEFAULT_DJANGO_SETTINGS_MODULE = 'config.settings'
 
 
@@ -9,3 +11,6 @@ def configure(django_settings_module=DEFAULT_DJANGO_SETTINGS_MODULE):
     os.environ.setdefault("DJANGO_BASE_DIR", os.path.dirname(os.path.abspath((inspect.stack()[1])[1])))
 
     from django_zero.config import settings
+
+
+__version__ = __version__
