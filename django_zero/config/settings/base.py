1aaf806aefec6c7b1b496adc71f49d1d9a2cc2cc
Romain Dorgueil <romain@dorgueil.net>
2018-06-02 10:03:58 +0200

    Recursive attempt to detect base directory instead of naive getcwd().

--- django_zero/config/settings/base.py
+++ django_zero/config/settings/base.py
@@ -1,4 +1,14 @@
 import os
 
-BASE_DIR = os.environ.get('DJANGO_BASE_DIR', os.getcwd())
+if os.environ.get('DJANGO_BASE_DIR'):
+    BASE_DIR = os.environ.get('DJANGO_BASE_DIR')
+else:
+    BASE_DIR = os.getcwd()
+    while not os.path.exists(os.path.join(BASE_DIR, 'setup.py')):
+        BASE_DIR = os.path.dirname(BASE_DIR).rstrip('/')
+        if not len(BASE_DIR):
+            raise OSError(
+                'Could not find django zero project base directory form cwd, please provide a DJANGO_BASE_DIR value (in env).'
+            )
+
 ZERO_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
