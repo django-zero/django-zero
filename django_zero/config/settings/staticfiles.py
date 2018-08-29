b809e3c6e3b76e84d3475080b7ab33959edd59de
Romain Dorgueil <romain@dorgueil.net>
2018-08-29 13:53:14 +0200

    Conditionnaliy enable whitenoise specific staticfiles storage if feature is enabled.

--- django_zero/config/settings/staticfiles.py
+++ django_zero/config/settings/staticfiles.py
@@ -2,6 +2,7 @@
 
 import os
 
+from django_zero.config.settings.features import is_whitenoise_enabled
 from django_zero.utils import create_directories_or_ignore
 
 from .base import BASE_DIR, ZERO_DIR
@@ -11,7 +12,11 @@ STATICFILES_DIRS = create_directories_or_ignore(
     os.path.join(BASE_DIR, ".cache/webpack"),
     os.path.join(ZERO_DIR, "resources/static"),
 )
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+
+if is_whitenoise_enabled():
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+else:
+    STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = os.environ.get("STATIC_URL", "/static/")
